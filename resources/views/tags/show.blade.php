1f73f6f275b829957c225930fa9757288c5a02f6
RizkiAgung007 <client689k@gmail.com>
2025-11-14 17:02:39 +0700

    final update

--- resources/views/tags/show.blade.php
+++ resources/views/tags/show.blade.php
@@ -6,13 +6,13 @@
             </h2>
             <a href="{{ route('tags.index') }}" class="inline-flex items-center px-4 py-2 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-500 rounded-md font-semibold text-xs text-gray-700 dark:text-gray-300 uppercase tracking-widest shadow-sm hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
                 <x-heroicon-o-arrow-left class="w-4 h-4 me-2"/>
-                {{ __('Back to All Tags') }}
+                {{ __('Back') }}
             </a>
         </div>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+        <div class="max-w-screen-2xl mx-auto sm:px-6 lg:px-8 space-y-6">
 
             <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
